447bddbcfffcdb498d023ca6c2afa09d19d74796
Raphael Cassari Benedicto <raphacassari@gmail.com>
2021-08-19 16:38:06 -0300

    heroku test

--- web/src/axios.js
+++ web/src/axios.js
@@ -24,7 +24,7 @@ export const apiCand = {
     create: async({ request }) => {
         try {
             const response = await api.post(
-                `http://localhost:3000/user/create`,
+                `${HEROKU_URL}}/user/create`,
                 request
             )
             return response.data;
