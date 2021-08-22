f93846fd7d25aa77a9e910b5189db49ebde2e852
Raphael Cassari Benedicto <raphacassari@gmail.com>
2021-08-22 14:26:48 -0300

    WIP: UX

--- web/src/axios.js
+++ web/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({}),
-    //BASE_URL = "https://api-shopping-cidadao-2.herokuapp.com"
-    BASE_URL = "http://localhost:3000"
+    BASE_URL = "https://api-shopping-cidadao-2.herokuapp.com"
+    //BASE_URL = "http://localhost:3000"
 
 export const apiMain = {
     get: async(request) => {
@@ -23,32 +23,17 @@ export const apiMain = {
 };
 
 export const apiCourses = {
-    /*     get: async() => {
-            try {
-                const response = await api.get(
-                    `${BASE_URL}/course`
-                )
-                return response.data.map((c) => ({
-                    value: c.n.id,
-                    text: c.n.name,
-                }));
-            } catch (error) {
-                return error
-            }
-        }, */
-    /*     getLanguages: async() => {
-            try {
-                const response = await api.get(
-                    `${BASE_URL}/course/languages`
-                )
-                return response.data.map((c) => ({
-                    value: c.n.id,
-                    text: c.n.name,
-                }));
-            } catch (error) {
-                return error
-            }
-        }, */
+    create: async({ request }) => {
+        try {
+            const response = await api.post(
+                `${BASE_URL}/user/create`,
+                request
+            )
+            return response.data;
+        } catch (error) {
+            return error
+        }
+    },
 };
 
 
